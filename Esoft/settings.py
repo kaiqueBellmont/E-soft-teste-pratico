a9a2d22a81105298fa3ca28724281fae327dd2c2
kaique Bellmont <kaiquebellmont@gmail.com>
2021-05-10 16:44:33 -0300

    teste

--- Esoft/settings.py
+++ Esoft/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+DISABLE_COLLECTSTATIC=1
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
