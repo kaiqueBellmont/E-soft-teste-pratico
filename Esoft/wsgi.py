a682bee3998195151e0defde2bc13affef4bb913
kaique Bellmont <kaiquebellmont@gmail.com>
2021-05-10 16:41:58 -0300

    teste

--- Esoft/wsgi.py
+++ Esoft/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Esoft.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Esoft.settings')
 
 application = get_wsgi_application()
